5b50ae0bc0ccadbf91821bd72ff21ca697baecac
patrykrosenkiewicz <patrykrosenkiewicz@gmail.com>
2022-01-23 20:31:11 +0100

    removed commented console.log

--- utils/movie/moviesSort.js
+++ utils/movie/moviesSort.js
@@ -1,7 +1,6 @@
 import { intersection } from 'lodash-es';
 
 export const sortMoviesByAmoutOfGenresMatch = (movies, genres)=> {
-    // console.log('first not sorted', movies[0]);
     let sortedMovies = movies.sort((a,b) => {
         const intersectionA = intersection(a.genres, genres);
         const intersectionB = intersection(b.genres, genres);
